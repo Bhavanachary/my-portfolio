9a5a358d89d2620eda7e2f07280aebd83b8200f2
Bhavana <bhavanak2408@gmail.com>
2025-09-04 13:16:09 -0400

    myprojectchanges

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ function Header() {
         animate={{ opacity: 1 }}
         transition={{ delay: 0.7, duration: 0.6 }}
       >
-        Front-End Developer | Angular | React | JavaScript | Azure
+        UI/UX Designer | Angular | React | JavaScript | Azure
       </motion.p>
     </motion.header>
   );
